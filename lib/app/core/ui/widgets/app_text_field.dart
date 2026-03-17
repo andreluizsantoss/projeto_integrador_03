94cbb0edb5d6ccb64d074299bd67bbf9a8bf3589
andreluizsantoss <andre.luiz.itape@gmail.com>
2026-03-17 13:58:44 -0300

    Implementação - AppTextField

---
+++ lib/app/core/ui/widgets/app_text_field.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import '../app_colors.dart';
+import '../app_text_styles.dart';
+
+class AppTextField extends StatefulWidget {
+  final String label;
+  final TextEditingController? controller;
+  final FormFieldValidator<String>? validator;
+  final bool obscureText;
+  final TextInputType keyboardType;
+  final Widget? suffixIcon;
+  final ValueChanged<String>? onChange;
+
+  const AppTextField({
+    super.key,
+    required this.label,
+    this.controller,
+    this.validator,
+    this.obscureText = false,
+    this.keyboardType = TextInputType.text,
+    this.suffixIcon,
+    this.onChange,
+  });
+
+  @override
+  State<AppTextField> createState() => _AppTextFieldState();
+}
+
+class _AppTextFieldState extends State<AppTextField> {
+  late bool _obscureText;
+
+  @override
+  void initState() {
+    super.initState();
+    _obscureText = widget.obscureText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          widget.label,
+          style: AppTextStyles.labelLarge.copyWith(
+            color: AppColors.onBackground.withValues(alpha: 0.8),
+          ),
+        ),
+        const SizedBox(height: 8),
+        TextFormField(
+          controller: widget.controller,
+          validator: widget.validator,
+          obscureText: _obscureText,
+          keyboardType: widget.keyboardType,
+          onChanged: widget.onChange,
+          style: AppTextStyles.bodyLarge,
+          decoration: InputDecoration(
+            hintText: 'Digite aqui...',
+            suffixIcon: widget.obscureText
+                ? IconButton(
+                    icon: Icon(
+                      _obscureText ? Icons.visibility_off : Icons.visibility,
+                      color: AppColors.grey,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _obscureText = !_obscureText;
+                      });
+                    },
+                  )
+                : widget.suffixIcon,
+          ),
+        ),
+      ],
+    );
+  }
+}
