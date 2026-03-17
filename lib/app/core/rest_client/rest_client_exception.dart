30e886ec1106c65eb242a3858f1727a6ffb27e01
andreluizsantoss <andre.luiz.itape@gmail.com>
2026-03-17 14:00:29 -0300

    Implementação - RestClientException

---
+++ lib/app/core/rest_client/rest_client_exception.dart
@@ -0,0 +1,18 @@
+class RestClientException implements Exception {
+  final String? message;
+  final int? statusCode;
+  final dynamic error;
+  final dynamic response;
+
+  RestClientException({
+    this.message,
+    this.statusCode,
+    this.error,
+    this.response,
+  });
+
+  @override
+  String toString() {
+    return 'RestClientException(message: $message, statusCode: $statusCode, error: $error, response: $response)';
+  }
+}
